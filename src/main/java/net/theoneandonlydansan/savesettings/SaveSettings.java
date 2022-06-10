c439acaad6b660bbefdc3d95bca39d0142305b59
TheOneAndOnlyDanSan <87494724+TheOneAndOnlyDanSan@users.noreply.github.com>
2022-06-10 23:05:25 +0300

    Add files via upload

--- src/main/java/net/theoneandonlydansan/savesettings/SaveSettings.java
+++ src/main/java/net/theoneandonlydansan/savesettings/SaveSettings.java
@@ -7,6 +7,7 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.text.Text;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
@@ -17,30 +18,32 @@ public class SaveSettings implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		writeToFile();
-		register();
+		try {
+			Files.write(options.toPath(), Files.readAllLines(config.toPath()));
+			register();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
-	public static void writeToFile() {
+	public static void saveSettings()  {
 		try {
 			if (!config.exists()) {
 				config.createNewFile();
-				Files.write(config.toPath(), Files.readAllLines(options.toPath()));
 			}
+			Files.write(config.toPath(), Files.readAllLines(options.toPath()));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 
 	public static void register() {
-		ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> {
-			dispatcher.register(
-				ClientCommandManager.literal("savesettings").executes(context -> {
-					writeToFile();
-					MinecraftClient.getInstance().player.sendMessage(Text.literal("settings saved"), true);
-					return 1;
-				})
-			);
-		});
+		ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> dispatcher.register(
+			ClientCommandManager.literal("savesettings").executes(context -> {
+				saveSettings();
+				MinecraftClient.getInstance().player.sendMessage(Text.literal("settings saved"), true);
+				return 1;
+			})
+		));
 	}
 }
