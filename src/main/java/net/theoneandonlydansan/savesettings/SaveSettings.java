62deb896699168489479c191a879344d4a9740f1
TheOneAndOnlyDanSan <87494724+TheOneAndOnlyDanSan@users.noreply.github.com>
2022-07-05 17:35:33 +0300

    Add files via upload

--- src/main/java/net/theoneandonlydansan/savesettings/SaveSettings.java
+++ src/main/java/net/theoneandonlydansan/savesettings/SaveSettings.java
@@ -5,24 +5,27 @@ import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.text.Text;
-
+import org.apache.commons.io.FileUtils;
 import java.io.File;
 import java.nio.file.Files;
-import java.nio.file.Path;
 
 public class SaveSettings implements ModInitializer {
-	static Path MinecraftDir = Path.of(System.getenv("APPDATA") + "/.minecraft");
-	static File config = new File(MinecraftDir + "/config/Save-Settings.txt");
+	static File MinecraftDir = new File(System.getenv("APPDATA") + "/.minecraft");
+	static File configFolderDir = new File(MinecraftDir + "/config");
+	static File saveFolderDir = new File(MinecraftDir + "/SaveSettings");
+	static File optionsSave = new File(saveFolderDir + "options.txt");
 	static File options = new File(MinecraftDir + "/options.txt");
 
 	@Override
 	public void onInitialize() {
 		try {
-			if (!config.exists()) {
-				config.createNewFile();
-				Files.write(config.toPath(), Files.readAllLines(options.toPath()));
+			if(!configFolderDir.exists()) {
+				configFolderDir.mkdir();
 			}
-			Files.write(options.toPath(), Files.readAllLines(config.toPath()));
+			if (!optionsSave.exists()) {
+				optionsSave.createNewFile();
+				saveSettings();
+			}
 			register();
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -31,15 +34,20 @@ public class SaveSettings implements ModInitializer {
 
 	public static void register() {
 		ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> dispatcher.register(
-			ClientCommandManager.literal("savesettings").executes(context -> {
-				try {
-					Files.write(config.toPath(), Files.readAllLines(options.toPath()));
+				ClientCommandManager.literal("SaveSettings").executes(context -> {
+					saveSettings();
 					MinecraftClient.getInstance().player.sendMessage(Text.literal("settings saved"), true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-				return 1;
-			})
+					return 1;
+				})
 		));
+	}
+
+	public static void saveSettings() {
+		try {
+			Files.write(optionsSave.toPath(), Files.readAllLines(options.toPath()));
+			FileUtils.copyDirectory(configFolderDir, saveFolderDir);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
