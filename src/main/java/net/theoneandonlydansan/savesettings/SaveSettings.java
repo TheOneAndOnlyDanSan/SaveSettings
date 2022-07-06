96b02241be3a893d6018d3a4d51d1837cea8ba91
TheOneAndOnlyDanSan <87494724+TheOneAndOnlyDanSan@users.noreply.github.com>
2022-07-06 08:09:54 +0300

    Add files via upload

--- src/main/java/net/theoneandonlydansan/savesettings/SaveSettings.java
+++ src/main/java/net/theoneandonlydansan/savesettings/SaveSettings.java
@@ -6,7 +6,10 @@ import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallba
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.text.Text;
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 
 public class SaveSettings implements ModInitializer {
 	static File MinecraftDir = new File(System.getenv("APPDATA") + "/.minecraft");
@@ -45,7 +48,7 @@ public class SaveSettings implements ModInitializer {
 	public static void saveSettings() {
 		try {
 			Files.write(optionsSave.toPath(), Files.readAllLines(options.toPath()));
-			Files.copy(configFolderDir.toPath(), saveFolderDir.toPath());
+			copyDir(configFolderDir.toPath(), saveFolderDir.toPath());
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -54,9 +57,19 @@ public class SaveSettings implements ModInitializer {
 	public static void LoadSettings() {
 		try {
 			Files.write(options.toPath(), Files.readAllLines(optionsSave.toPath()));
-			Files.copy(saveFolderDir.toPath(), configFolderDir.toPath());
+			copyDir(saveFolderDir.toPath(), configFolderDir.toPath());
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+
+	public static void copyDir(Path src, Path dest) throws Exception {
+		Files.walk(src).forEach(source -> {
+			try {
+				Files.copy(source, dest.resolve(src.relativize(source)), StandardCopyOption.REPLACE_EXISTING);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		});
 	}
 }
